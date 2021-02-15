6a9446fdededda4c06ae366413a0c36bb6e789cd
Ryan Kaszubski <gn2289@wayne.edu>
2021-02-15 17:37:49 -0500

    added context for my functions regarding the database

--- app/src/main/java/com/DegreeSchedulerApp/degreescheduler/MainActivity.java
+++ app/src/main/java/com/DegreeSchedulerApp/degreescheduler/MainActivity.java
@@ -18,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //This is a test for the first project demo
+        //We can add more users using a constructor like this:
+        //we can pull the data from the text boxes a user would type in
         UserInformation user = new UserInformation("gn2289", "1234", "gn2289@wayne.edu", 1);
 
+        //Database helper is used to manipulate the database
+        //this is were we can so far add new users
         DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
+        //here is how we can add a new user:
         dataBaseHelper.add(user);
 
 
