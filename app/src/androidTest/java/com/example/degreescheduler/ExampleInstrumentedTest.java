140c7e57585af87c76b46d2df468a6283cb63c69
“CiahGreen” <green.ciah@gmail.com>
2021-04-15 22:51:44 -0400

    Ciah Green Junit

--- app/src/androidTest/java/com/example/degreescheduler/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/degreescheduler/ExampleInstrumentedTest.java
@@ -2,8 +2,9 @@ package com.example.degreescheduler;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+
+import com.DegreeSchedulerApp.degreescheduler.Data.ClassInfo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +16,51 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.degreescheduler", appContext.getPackageName());
+    public void testcrn() {
+        ClassInfo classInfo = new ClassInfo();
+        classInfo.setCrn("0");
+        assert(classInfo.getCrn().equals("0")); // this verifies to whatever we want
     }
+
+    @Test
+    public void testclassName() {
+        ClassInfo classInfo= new ClassInfo();
+        classInfo.setClassName("Name");
+        assert (classInfo.getClassName().equals("Name"));
+    }
+
+
+    @Test
+    public void testclassNumber() {
+        ClassInfo classInfo= new ClassInfo();
+        classInfo.setClassNumber("Number");
+        assert (classInfo.getClassNumber().equals("Number"));
+    }
+
+    @Test
+    public void teststartDate() {
+        ClassInfo classInfo= new ClassInfo();
+        classInfo.setStartDate("Date");
+        assert (classInfo.getStartDate().equals("Date"));
+
+    }
+    @Test
+    public void testendDate() {
+        ClassInfo classInfo= new ClassInfo();
+        classInfo.setEndDate("End Date");
+        assert (classInfo.getEndDate().equals("End Date"));
+
+    }
+
+    @Test
+    public void testdays() {
+        ClassInfo classInfo= new ClassInfo();
+        classInfo.setDays("Days");
+        assert (classInfo.getDays().equals("Days"));
+
+    }
+
 }
